3f02af2facff6837a16aec11b22d5c297a330f19
shivanimonga <shivanimonga94@gmail.com>
2017-07-12 15:44:23 +0530

    project completed

---
+++ app/src/test/java/com/example/shivani/shivani/Adapters/UserAdapterTest.java
@@ -0,0 +1,40 @@
+/*package com.example.shivani.shivani.Adapters;
+
+import com.example.shivani.shivani.Model.Users;
+
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by shivani on 12/7/17.
+ */
+/*public class UserAdapterTest {
+    UserAdapter userAdapter;
+    ArrayList<Users> user;
+    Users user1=new Users();
+
+    @Mock
+    UserAdapter.ItemClickListener mockitemClickListener;
+
+    @Test
+    public void removeItem() throws Exception {
+        user1.setId(1); user1.setTitle("one"); user1.setUrl("www.google.com");
+        user.add(0,user1);
+
+        userAdapter.setClickListener(mockitemClickListener);
+        userAdapter.authenticateData(user);
+        assert
+    }
+
+    @Test
+    public void addToFavourite() throws Exception {
+
+    }
+
+
+}
+*/
---
+++ app/src/test/java/com/example/shivani/shivani/MainActivityTest.java
@@ -0,0 +1,35 @@
+package com.example.shivani.shivani;
+
+import android.view.View;
+
+import com.example.shivani.shivani.Model.Users;
+
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by shivani on 12/7/17.
+ */
+public class MainActivityTest {
+    @Mock
+    Users mockUser;
+
+
+    @Test
+    public void onClick() throws Exception {
+
+    }
+
+    @Test
+    public void deleteItem() throws Exception {
+
+    }
+
+    @Test
+    public void favourite() throws Exception {
+
+    }
+
+}
